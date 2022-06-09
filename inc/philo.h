c277e6331f571ff99df90ddca6339a14e867b142
AsheraHerlinda <94595638+AsheraHerlinda@users.noreply.github.com>
2022-06-09 16:37:46 +0400

    Add files via upload

--- inc/philo.h
+++ inc/philo.h
@@ -13,9 +13,37 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef pthread_mutex_t mutex;
+
+typedef struct s_philo
+{
+	int				posit;
+	int				eat_cnt;
+	long long int	t_last_eat;
+	mutex			*left_fork;
+	mutex			*right_fork;
+	pthread_t		philo_pth;		
+}	t_philo;
+
 typedef struct s_data
 {
-	int	phils;
+	int				n_phils;
+	int				n_forks;
+	int				t_die;
+	int				t_eat;
+	int				t_sleep;
+	int				must_eat;
+	t_philo			**philos;
+	mutex			*m_forks;
+	mutex			m_printf;
+	long long int	t_begin;
+	pthread_t		checker;
 }	t_data;
 
 int		error_msg(char *errmsg, int errnum);
--- man/ philo.c
+++ man/ philo.c
@@ -14,8 +14,23 @@
 
 int	main(int argc, char argv)
 {
+	t_data	*data;
+
 	if (argc < 5 || argv > 6)
 		if ((error_msg("Incorrect nums of arguments", 0) == -1))
 			return (-1);
+	if ((data_init(data)) == -1)
+		return (-1);		
 	return (0);
+}
+
+static t_data	*data_init(void)
+{
+	t_data	*data;
+
+	data = (t_data *) malloc (sizeof (t_data));
+	if (!data)
+		if ((error_msg("data malloc error", 0) == -1))
+			return (-1);
+	
 }
